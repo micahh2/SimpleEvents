5a8d729be191be8f518d0f13b2249518cdcbdcda
Micah <micah@dvyld.com>
2016-11-28 01:10:42 -0600

    Mostly done.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ import { Router, Route, Link, browserHistory } from 'react-router'
 import request from 'superagent';
 import * as _ from 'lodash';
 
-const categories = { "Fun": 0, "Sad": 1, "Normal": 2, "Cancelled": 3, };
+const categories = { "Fun": 0, "Sad": 1, "Normal": 2, "Canceled": 3, };
 let events = [];
 const fetch = (r) => {
   request.get('/list').then((resp) => {
@@ -13,20 +13,27 @@ const fetch = (r) => {
   });
 }
 
-const EventSummary = React.createClass({
-  onClick(e) {
-    console.log('Click!');
+const store = {
+  register(app) {
+    this.app = app;
   },
+  updateState() {
+    this.app.setState(this.app.state);
+  },
+}
+
+
+const EventSummary = React.createClass({
   render() {
     let ev = this.props.event;
-    return <div onClick={ this.onClick }>
+    return <Link to={ `/event/${ev.id}` }>
       <h2>{ ev.title }</h2>
       <p>
         { ev.description }
         <br/><i>Created: { ev.createdAt }</i>
-        <br/><i>Updated: { ev.createdAt }</i>
+        <br/><i>Updated: { ev.updatedAt }</i>
       </p>
-    </div>  
+    </Link>  
   },
 });
 
@@ -35,33 +42,74 @@ const NoMatch = React.createClass({
     return <h2>Not Found :(</h2>
   },
 });
-    
+   
 const DetailView = React.createClass({
+  deleteEvent() {
+    let index = events.indexOf(this.state.event);
+    events.splice(index, 1);
+    request.post('/delete').send({id: this.state.event.id}).end();
+    //browserHistory.push('/');
+  },
+  updateEvent(key, filter) {
+    let dv = this; 
+    return (e) => {
+      let val = e.target.value;
+      if(e.target.type === 'checkbox')
+        val = e.target.checked;
+      if(typeof filter === 'function')
+        val = filter(e.target.value);
+      this.state.event[key] = val;
+      this.state.event.updatedAt = new Date();
+      dv.setState(this.state);
+      store.updateState();
+      clearTimeout(this.queuedReq);
+      this.queuedReq = setTimeout(() => {
+        request.post('/update').send(this.state.event).end();
+      }, 500);
+    };
+  },
   getInitialState() {
     let id = parseInt(this.props.params.id);
     let event = _.find(events, { id });
-    return {
-      event: {...event}
-    }
+    return { event, }
   },
   render() {
     let ev = this.state.event;
     let radio = [];
-    for(let i of categories.map((val, key) => { return { val, key }; }))
-      radio.push(<input type="radio" value="category" checked={ ev == i.key}>i.val</input>)
-
+    for(let i of _.map(categories, (val, key) => { return { val, key }; })) {
+      radio.push(<input 
+          type="radio" 
+          name="category" 
+          checked={ ev.category === i.val } 
+          onChange={ this.updateEvent('category', parseInt) } 
+          value={ i.val }/>);
+      radio.push(<label>{ i.key }</label>);
+    }
     return <div>
-      <input type="text" value={ ev.title }/>
-      <br/><textarea value={ ev.description | ""}></textarea>
-      <br/><input type="button" value="Submit"/>
+      <input type="text" value={ ev.title } onChange={ this.updateEvent('title') }/>
+      <br/><textarea value={ ev.description } onChange={ this.updateEvent('description') }></textarea>
+      <br/><label>Start Date</label>
+      <br/><input type="date" value={ ev.startDate } onChange={ this.updateEvent('startDate') }/>
+      <br/><label>End Date</label>
+      <br/><input type="date" value={ ev.endDate } onChange={ this.updateEvent('endDate') }/>
       <br/>{ radio }
+      <br/><input type="checkbox" checked={ !!ev.featured } onChange={ this.updateEvent('featured') }/><label>Featured</label>
+      <br/><Link to="/">Go Back</Link> | <Link to="/" onClick={ this.deleteEvent }>Delete Event</Link>
       <br/><i>Created: { ev.createdAt }</i>
-      <br/><i>Updated: { ev.createdAt }</i>
+      <br/><i>Updated: { ev.updatedAt }</i>
     </div>  
   },
 });
 
 const ListView = React.createClass({
+  addEvent() {
+    request.post('/add').then((resp) => {
+      events.push(resp.body);
+      store.updateState();
+      browserHistory.push(`/event/${ resp.body.id }`);
+      return false;
+    });
+  },
   getInitialState() {
     return { events };
   }, 
@@ -69,20 +117,27 @@ const ListView = React.createClass({
     let rows = [];
     for (let event of this.state.events) {
       rows.push(<li>
-        <Link to={ `/event/${event.id}` }>
-          <EventSummary event={ event }/>
-        </Link>
+        <EventSummary event={ event }/>
       </li>);
     }
-    return <ul> {rows} </ul>
+    return <div>
+             <ul> {rows} </ul>
+             <a href="#addEvent" onClick={ this.addEvent }>Add Event</a>
+           </div>
   },
 });
 
 const App = React.createClass({
+  getInitialState() {
+    return { events };
+  },
+  componentWillMount() {
+    store.register(this);
+  },
   render() { 
     let body = this.props.children;
     if(!body) 
-      body = <ListView {...this.props}/>
+      body = <ListView { ...this.props }/>
 
     return <div>
       <h1>Events</h1>
@@ -96,10 +151,10 @@ const render = () => {
       <Router history={ browserHistory }>
         <Route path="/" component={ App }>
           <Route path="event/:id" component={ DetailView }/>
-          <Route path="*" component={NoMatch}/>
+          <Route path="*" component={ NoMatch }/>
         </Route>
       </Router>,
       document.getElementById('container'));
-}
+};
 
 fetch(render);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const router = require('express').Router();
+const router = express.Router();
+const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -7,6 +8,8 @@ const Sequelize = require('sequelize');
 
 // App
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 const runApp = function () {
   app.listen(8080, function () {
       console.log('App Listing On 8080');
@@ -28,11 +31,24 @@ router.get('/list', function (req, res) {
       res.json(data);
     });
 });
+router.post('/update', function(req, res) {
+  Event.update(req.body, { where: { id: req.body.id, }, });
+  res.status(200).end();
+});
+router.post('/delete', function(req, res) {
+  Event.destroy({ where: { id: req.body.id, }, });
+  res.status(200).end();
+});
+router.post('/add', function(req, res) {
+  var ev = Event.build(Object.assign({}, req.body));
+  ev.save().then(() => res.json(ev).end());
+});
 router.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.use('/bin', express.static('bin'));
 app.use('/', router);
+
 
 //Start
 try {
